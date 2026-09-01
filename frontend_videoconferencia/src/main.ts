8f879242397ff54e0a11ab7e312b76099e92b2a8
PanjojLuis8944 <lpanjojx@miumg.edu.gt>
2026-08-31 22:38:58 -0600

    Agregacion de Bootstrap

--- frontend_videoconferencia/src/main.ts
+++ frontend_videoconferencia/src/main.ts
@@ -1,4 +1,7 @@
 import './assets/main.css'
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'

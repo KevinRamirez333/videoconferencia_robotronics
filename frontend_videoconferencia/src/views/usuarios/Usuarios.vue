b793e0d052eee01136e307fbdd00a529118474ac
Kevin Ramirez <kevinramirez2467@gmail.com>
2026-09-01 13:36:20 -0600

    Ajuste de frontend

---
+++ frontend_videoconferencia/src/views/usuarios/Usuarios.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+const crearUsuario = async () => {
+  error.value = '';
+  loading.value = true;
+
+  try {
+    const response = await api.post('/usuarios', {
+      nombre_usuario: nombre_usuario.value,
+      password: password.value,
+    });
+ } catch (err) {
+    error.value = 'Usuario o contraseña incorrectos.';
+};
+}
+</script>
+
+<template>
+
+
+</template>

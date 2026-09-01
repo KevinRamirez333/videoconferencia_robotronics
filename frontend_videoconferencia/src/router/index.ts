614593a0f3f4101fc27f13b580c091d01ecbda63
CasiaProgrammer <tu-correo@gmail.com>
2026-08-31 21:17:57 -0600

    Login Ejemplo

--- frontend_videoconferencia/src/router/index.ts
+++ frontend_videoconferencia/src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '@/views/login/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: Login,
     },
     {
       path: '/about',
@@ -17,6 +18,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
   ],
 })
 

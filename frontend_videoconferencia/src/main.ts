54f4d875635eba1346ebb0d061b1c1c034f3710a
CasiaProgrammer <tu-correo@gmail.com>
2026-08-31 22:38:55 -0600

    Agregacion de bootstrap

--- frontend_videoconferencia/src/main.ts
+++ frontend_videoconferencia/src/main.ts
@@ -3,6 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap';
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp(App)
 

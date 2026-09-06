fdc0a58122c22df4f0a4d2ed97fece06c2dcb641
CasiaProgrammer <tu-correo@gmail.com>
2026-09-05 18:39:55 -0600

    Planificacion de sesiones creadas y funcionando

--- backend_videoconferencia/src/modulos/sesiones/sesion.rutas.ts
+++ backend_videoconferencia/src/modulos/sesiones/sesion.rutas.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { listarSesiones, crearSesion, actualizarSesion } from './sesion.controlador';
+import { listarSesiones, crearSesion } from './sesion.controlador';
 
 const router = Router();
 
 router.get('/', listarSesiones);
 router.post('/', crearSesion);
-router.put('/:id', actualizarSesion);
 
 export default router;

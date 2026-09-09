d5fee9164afe0a4ec6f589d096970965b42eaa71
Lester Hernandez <lhernandeza24@miumg.edu.gt>
2026-09-09 03:40:21 -0600

    Creacion de la vista de sala

--- frontend_videoconferencia/src/router/index.ts
+++ frontend_videoconferencia/src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Sala from '../views/sala/sala.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-
+    {
+      path: '/',
+      redirect: { name: 'sala' },
+    },
+    {
+      path: '/sala',
+      name: 'sala',
+      component: Sala,
+    },
   ],
 })
 
